26020e7e0e9c9d1ff11672070b4110d8dadea193
James <Jamesgibson14@hotmail.com>
2012-08-11 22:08:47 -0600

    static server working

--- node/staticServer.js
+++ node/staticServer.js
@@ -1,55 +1,37 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var io = require('socket.io').listen(app);
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+    port = 2389;
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+http.createServer(function(request, response) {
 
-http.createServer(function (request, response) {
- 
-    console.log('request starting...');
-     
-    var filePath =  request.url;
-    if (filePath == '/')
-        filePath = '../index.html';
-    filePath = '../' + filePath; 
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+  
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
     }
-    console.log(filePath);
-    fs.exists(filePath, function(exists) {
-     
-        if (exists) {
-            fs.readFile(filePath, function(error, content) {
-                if (error) {
-                    response.writeHead(500);
-                    response.end();
-                }
-                else {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                }
-            });
-        }
-        else {
-        	console.log('errror: ' + filePath);
-            response.writeHead(404);
-            response.end();
-        }
+
+	if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
     });
-     
-}).listen(80);
- 
-console.log('Server running at http://127.0.0.1:80/');
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
