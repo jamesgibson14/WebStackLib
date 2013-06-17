871d1c7194ceef1cfc60f2605760562efc83fd1c
James Gibson <jamesgibson14@hotmail.com>
2013-06-17 11:33:06 -0600

    created data entry form

---
+++ js/models/ItemVerificationDataEntry.js
@@ -0,0 +1,34 @@
+define(['jquery', 'backbone','engine', 'models/BaseADODBModel'], function($, Backbone,E, BaseADOModel) {
+
+var Todo = BaseADOModel.extend({
+    idAttribute: "ID",
+    
+    urlRoot : '/ItemVerification',
+    initialize: function() {
+
+    },
+    
+    parse: function(res){
+        if(typeof(res.Completed) ==='date')
+            res.Completed = new Date(res.Completed)
+        if(typeof(res.DueAt) ==='date')
+            res.DueAt = new Date(res.DueAt)
+        return res
+    },
+    onBeforeCreate: function(options){
+        if (!this.get('Options')){
+            var op = this.get('Options') || {}
+            op.LP = this.collection.nextOrder()
+            this.set({Options: op},{silent:true})
+        }
+        if(!this.get('AssignedTo'))
+            this.set('AssignedTo',E.user.get('Associate_ID'),{silent:true})
+        this.set({Created: new Date(), CreatedBy: E.user.get('Associate_ID')},{silent:true})
+    }
+
+  });
+
+    // Returns the Model class
+    return Todo;
+
+});
